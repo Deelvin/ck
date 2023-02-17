c89c50161eb5c9742d60a362c6a5f86f41e678b0
Ailurus1 <ilyako25@gmail.com>
2023-02-17 08:42:07 +0000

    Started introducing generic get-tvm-model script

--- cm-mlops/script/get-tvm-model/process.py
+++ cm-mlops/script/get-tvm-model/process.py
@@ -1,9 +1,10 @@
 import os
 import tvm
-# Import model to TVM
 from tvm import relay
+
 max_batchsize = os.environ['CM_ML_MODEL_MAX_BATCH_SIZE']
 input_shapes = os.environ.get('CM_ML_MODEL_INPUT_SHAPES','').strip()
+
 if input_shapes == '':
     print ('')
     raise Exception("Error: CM_ML_MODEL_INPUT_SHAPES environment variable is not defined!")
