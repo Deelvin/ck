f5f93f1378afd1fb5a19af239ece69c5961495b5
Ailurus1 <ilyako25@gmail.com>
2023-05-08 08:09:42 +0300

    Fix typo

--- cm-mlops/script/get-mlperf-tiny-src/customize.py
+++ cm-mlops/script/get-mlperf-tiny-src/customize.py
@@ -24,6 +24,6 @@ def postprocess(i):
     env["CM_MLPERF_TINY_AD_MODELS_PATH"] = os.path.join(os.getcwd(), "tiny", "benchmark", "training", "anomaly_detection", "trained_models")
     env["CM_MLPERF_TINY_IC_MODELS_PATH"] = os.path.join(os.getcwd(), "tiny", "benchmark", "training", "image_classification", "trained_models")
     env["CM_MLPERF_TINY_VWW_MODELS_PATH"] = os.path.join(os.getcwd(), "tiny", "benchmark", "training", "visual_wake_words", "trained_models")
-    env["CM_MLPERF_TINY_REFERENCE_SUBMISSION"] = os.path.join(os.getcwd(), "tiny", "benchmark", "reference_submission")
+    env["CM_MLPERF_TINY_REFERENCE_SUBMISSION"] = os.path.join(os.getcwd(), "tiny", "benchmark", "reference_submissions")
 
     return {'return':0}
